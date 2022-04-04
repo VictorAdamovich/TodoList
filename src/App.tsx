6fb4fd06e2073873a3037bd0e96432396bb37772
Victor Adamovich <ndptrv@yandex.by>
2022-04-04 20:59:54 +0300

    Update work

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,28 @@ import TodoList, {TaskType} from "./TodoList";
 //GUI
 //CLI
 //CRUD
+export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App() {
     //BLL
-    let [tasks, setTasks] = useState<Array<TaskType>>([
+    const TodoListTitle: string = 'What to learn';
+
+    const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: 'HTML', isDone: true},
         {id: 2, title: 'Css', isDone: true},
         {id: 3, title: 'JS/TS', isDone: false}
     ])
 
-    const TodoListTitle: string = 'What to learn';
-
     const removeTask = function (taskId: number) {
         setTasks(tasks.filter(t => t.id !== taskId))
     }
+
+    const [filter, setFilter] = useState<FilterValuesType>('all')
+    const changeFilter = (filter: FilterValuesType) => {
+        setFilter(filter)
+        console.log(filter)
+    }
+
 
     //UL
     return (
@@ -26,7 +34,10 @@ function App() {
             <TodoList
                 title={TodoListTitle}
                 tasks={tasks}
-                removeTask={removeTask}/>
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
+
         </div>
     );
 }
