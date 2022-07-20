a3629d9a89943d067384ad491fa2484930dc4a81
Victor Adamovich <ndptrv@yandex.by>
2022-07-20 19:48:23 +0300

    add hw lesson 16

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,26 +1,33 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios';
 
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+const settings = {
     withCredentials: true,
     headers: {
         'API-KEY': '52ef1d1d-e88b-4a8d-9961-355652d6dbd0'
     }
-})
+};
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    ...settings
+});
 
 // api
 export const todolistsAPI = {
     getTodolists() {
-        return instance.get<TodolistType[]>('todo-lists');
+        const promise = instance.get<TodolistType[]>('todo-lists');
+        return promise;
     },
     createTodolist(title: string) {
-        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TodolistType }>>>('todo-lists', {title});
+        const promise = instance.post<ResponseType<{ item: TodolistType }>>('todo-lists', {title: title});
+        return promise;
     },
     deleteTodolist(id: string) {
-        return instance.delete<ResponseType>(`todo-lists/${id}`);
+        const promise = instance.delete<ResponseType>(`todo-lists/${id}`);
+        return promise;
     },
     updateTodolist(id: string, title: string) {
-        return instance.put<{ title: string }, AxiosResponse<ResponseType>>(`todo-lists/${id}`, {title});
+        const promise = instance.put<ResponseType>(`todo-lists/${id}`, {title: title});
+        return promise;
     },
     getTasks(todolistId: string) {
         return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`);
@@ -28,13 +35,36 @@ export const todolistsAPI = {
     deleteTask(todolistId: string, taskId: string) {
         return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`);
     },
-    createTask(todolistId: string, title: string) {
-        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks`, {title});
+    createTask(todolistId: string, taskTitile: string) {
+        return instance.post<ResponseType<{ item: TaskType }>>(`todo-lists/${todolistId}/tasks`, {title: taskTitile});
     },
     updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
-        return instance.put<UpdateTaskModelType, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
+        return instance.put<ResponseType<TaskType>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
     }
+};
+
+
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe: boolean
+    captcha?: string
 }
+
+export const authAPI = {
+    login(data: LoginParamsType) {
+        const promise = instance.post<ResponseType<{ userId?: number }>>('auth/login', data);
+        return promise;
+    },
+    logout() {
+        const promise = instance.delete<ResponseType<{ userId?: number }>>('auth/login');
+        return promise;
+    },
+    me() {
+        const promise = instance.get<ResponseType<{ id: number; email: string; login: string }>>('auth/me');
+        return promise;
+    }
+};
 
 // types
 export type TodolistType = {
@@ -46,10 +76,8 @@ export type TodolistType = {
 export type ResponseType<D = {}> = {
     resultCode: number
     messages: Array<string>
-    fieldsErrors: Array<string>
     data: D
 }
-
 
 export enum TaskStatuses {
     New = 0,
--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,21 +1,17 @@
-import {setAppErrorAC, setAppStatusAC} from '../app/app-reducer';
-import {Dispatch} from 'redux';
-import {ResponseType} from '../api/todolists-api';
-import {ActionsType} from '../features/TodolistsList/tasks-reducer';
+import {setAppErrorAC, SetAppErrorActionType, setAppStatusAC, SetAppStatusActionType} from '../app/app-reducer'
+import {ResponseType} from '../api/todolists-api'
+import {Dispatch} from 'redux'
 
-// generic function
-export const handleServerAppError = <T>(data: ResponseType<T>, dispatch: Dispatch<ActionsType>) => {
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
     if (data.messages.length) {
-        dispatch(setAppErrorAC(data.messages[0]));
+        dispatch(setAppErrorAC(data.messages[0]))
     } else {
-        dispatch(setAppErrorAC('Some error occurred'));
+        dispatch(setAppErrorAC('Some error occurred'))
     }
-    dispatch(setAppStatusAC('failed'));
-};
+    dispatch(setAppStatusAC('failed'))
+}
 
-export const handleServerNetworkError = (message: string, dispatch: Dispatch<ActionsType>) => {
-    dispatch(setAppErrorAC(message));
-    dispatch(setAppStatusAC('failed'));
-};
-
-
+export const handleServerNetworkError = (error: { message: string }, dispatch: Dispatch<SetAppErrorActionType | SetAppStatusActionType>) => {
+    dispatch(setAppErrorAC(error.message ? error.message : 'Some error occurred'))
+    dispatch(setAppStatusAC('failed'))
+}
