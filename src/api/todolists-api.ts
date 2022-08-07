ed40a1c1a871fa1586d7fe3e44652c4cf7c2f8ed
Victor Adamovich <ndptrv@yandex.by>
2022-08-07 13:30:09 +0300

    add redux-toolkit and refactoring

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const settings = {
     withCredentials: true,
     headers: {
         'API-KEY': '52ef1d1d-e88b-4a8d-9961-355652d6dbd0'
     }
-};
+}
 const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     ...settings
-});
+})
 
 // api
 export const todolistsAPI = {
@@ -36,12 +36,12 @@ export const todolistsAPI = {
         return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`);
     },
     createTask(todolistId: string, taskTitile: string) {
-        return instance.post<ResponseType<{ item: TaskType }>>(`todo-lists/${todolistId}/tasks`, {title: taskTitile});
+        return instance.post<ResponseType<{ item: TaskType}>>(`todo-lists/${todolistId}/tasks`, {title: taskTitile});
     },
     updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
         return instance.put<ResponseType<TaskType>>(`todo-lists/${todolistId}/tasks/${taskId}`, model);
     }
-};
+}
 
 
 export type LoginParamsType = {
@@ -53,18 +53,18 @@ export type LoginParamsType = {
 
 export const authAPI = {
     login(data: LoginParamsType) {
-        const promise = instance.post<ResponseType<{ userId?: number }>>('auth/login', data);
+        const promise = instance.post<ResponseType<{userId?: number}>>('auth/login', data);
         return promise;
     },
     logout() {
-        const promise = instance.delete<ResponseType<{ userId?: number }>>('auth/login');
+        const promise = instance.delete<ResponseType<{userId?: number}>>('auth/login');
         return promise;
     },
     me() {
-        const promise = instance.get<ResponseType<{ id: number; email: string; login: string }>>('auth/me');
-        return promise;
+       const promise =  instance.get<ResponseType<{id: number; email: string; login: string}>>('auth/me');
+       return promise
     }
-};
+}
 
 // types
 export type TodolistType = {
@@ -78,14 +78,12 @@ export type ResponseType<D = {}> = {
     messages: Array<string>
     data: D
 }
-
 export enum TaskStatuses {
     New = 0,
     InProgress = 1,
     Completed = 2,
     Draft = 3
 }
-
 export enum TaskPriorities {
     Low = 0,
     Middle = 1,
@@ -93,7 +91,6 @@ export enum TaskPriorities {
     Urgently = 3,
     Later = 4
 }
-
 export type TaskType = {
     description: string
     title: string
